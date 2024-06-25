869cf718d83b6ec33c74f92d4c90bfeec79feaaa
aErica26 <ericanepomuceno43@gmail.com>
2024-06-25 16:23:58 -0300

    Solução dos exercícios

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 /* Você precisará implementar no carrinho de compras a soma do total dentro da função abaixo ja criada.*/
 
 document.addEventListener('DOMContentLoaded', function () {
-    
+    const cartItems = document.querySelectorAll("#cart-items .item");
+ 
+  let total = 0;
+ 
+  cartItems.forEach(function(item) {
+      const price = parseFloat(item.getAttribute("data-price"));
+ 
+      total += price;
+  });
+ 
+  document.getElementById("total-price").textContent = total.toFixed(2);
 });
